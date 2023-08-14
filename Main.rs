8b911ece26e14d7475e66b1fdcbe606291d27723
Tomin <140749649+tomin44@users.noreply.github.com>
2023-08-14 23:02:35 +0200

    Update Main.rs

--- Main.rs
+++ Main.rs
@@ -3,8 +3,8 @@ use std::io;
 fn main() {
     println!("Please enter your name:");
     
-    let mute name = String::new();
-    io::stdin().read_line(&mute name).expect("Failed to read line");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read line 1");
     
     println!("Hello, {}! Welcome to Rust programming.", name.trim());
 }
