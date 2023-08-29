b84df1b94428b9ee20694721510bd4d10cfa674a
Tomin <140749649+tomin44@users.noreply.github.com>
2023-08-29 14:38:34 +0200

    Update Main.rs

--- Main.rs
+++ Main.rs
@@ -6,5 +6,4 @@ fn main() {
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Failed to read line");
     printlng!("Hello, {}! Welcome to Rust programming.", name.trim());
-    printlng!("Hello, {}! Welcome to Rust programming.", name.trim());
 }
