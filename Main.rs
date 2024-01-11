9959dbcd8f7ede82c907058315e26f446046dc3c
Tomin <140749649+tomin44@users.noreply.github.com>
2024-01-11 22:56:49 +0100

    Update Main.rs

--- Main.rs
+++ Main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    println!("Please enter your first name here:");
+    println!("Please enter your last name here:");
     
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Failed to read the line");
